69e0d91dda04c74a17cc22e8917dfc123ec4da9d
anhlt570 <anhletuan128@gmail.com>
2017-02-23 18:00:08 +0700

    - Pull RSS of stackoverflow - create a webview in main layout

--- app/src/main/java/com/tuananh2/newspaper/MainActivity.java
+++ app/src/main/java/com/tuananh2/newspaper/MainActivity.java
@@ -6,6 +6,10 @@ import android.support.v4.content.ParallelExecutorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import org.xml.sax.helpers.XMLReaderFactory;
 import org.xmlpull.v1.XmlPullParser;
@@ -20,73 +24,40 @@ import java.net.URL;
 
 public class MainActivity extends Activity {
     public static final String TAG = "anhlt2";
-
+    XMLParser xmlParser ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        xmlParser = new XMLParser();
         fetchXML();
-
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new Browser());
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("");
     }
 
     public void fetchXML() {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                try {
-                    URL url = new URL("http://vnexpress.net/rss/thoi-su.rss");
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                    connection.setReadTimeout((15000));
-                    connection.setConnectTimeout(10000);
-                    connection.setRequestMethod("GET");
-                    connection.setDoInput(true);
-                    connection.connect();
-                    InputStream inputStream = connection.getInputStream();
-                    XmlPullParserFactory parserFactory = XmlPullParserFactory.newInstance();
-                    XmlPullParser parser = parserFactory.newPullParser();
-                    parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-                    parser.setInput(inputStream, null);
-                    String text = "";
-                    int event = parser.getEventType();
-                    while (event != XmlPullParser.END_DOCUMENT) {
 
-                        Log.d(TAG, "run: ");
-                        String name = parser.getName();
-                        switch (event) {
-                            case XmlPullParser.START_TAG: {
-                                break;
-                            }
-                            case XmlPullParser.TEXT: {
-                                text = parser.getText();
-                                break;
-                            }
-                            case XmlPullParser.END_TAG: {
-                                if (name.equals("title")) {
-                                    Log.d(TAG, "title: " + text);
-                                } else if (name.equals("link")) {
-                                    Log.d(TAG, "link: " + text);
-                                } else if (name.equals("pubDate")) {
-                                    Log.d(TAG, "public date: " + text);
-                                } else if (name.equals("description")) {
-                                    Log.d(TAG, "description: " + text);
-                                }
-                                break;
-                            }
-                        }
-                        parser.next();
-                        event=parser.getEventType();
-                    }
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
+                try {
+                    xmlParser.fetchXML("http://stackoverflow.com/feeds/tag?tagnames=android&sort=newest");
                 } catch (IOException e) {
                     e.printStackTrace();
-                } catch (XmlPullParserException e) {
-                    e.printStackTrace();
                 }
-
             }
         });
         thread.start();
+    }
+    private class Browser extends WebViewClient
+    {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
